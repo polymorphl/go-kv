e443c2f21f8c836692061ce90c97670a6bb6005b
Luc T <lucterracher@gmail.com>
2025-09-01 22:55:29 +0200

    Refactor main function to handle multiple connections and respond to PING commands

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -16,12 +18,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleConnection(conn)
 	}
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Write([]byte("+PONG\r\n"))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+	}
 }
