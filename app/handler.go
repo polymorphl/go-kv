fe12c34ed4a4e9ffc7352a317c563e64f5afa198
Luc T <lucterracher@gmail.com>
2025-09-03 10:40:02 +0200

    Add SET and GET commands to handle in-memory key-value storage

--- app/handler.go
+++ app/handler.go
@@ -3,7 +3,11 @@ package main
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 	"ECHO": echo,
+	"SET":  set,
+	"GET":  get,
 }
+
+var memory = make(map[string]string)
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -15,4 +19,18 @@ func ping(args []Value) Value {
 
 func echo(args []Value) Value {
 	return Value{Typ: "string", Str: args[0].Bulk}
+}
+
+func set(args []Value) Value {
+	memory[args[0].Bulk] = args[1].Bulk
+	return Value{Typ: "string", Str: "OK"}
+}
+
+func get(args []Value) Value {
+	tmp := memory[args[0].Bulk]
+	if tmp == "" {
+		return Value{Typ: "string", Str: ""}
+	}
+
+	return Value{Typ: "string", Str: tmp}
 }
